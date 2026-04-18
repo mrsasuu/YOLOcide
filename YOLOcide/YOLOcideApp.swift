f36d73b9d70adcd64ca3067cd02514a130270555
Antonio Benitez Guijarro <mrsasuu@gmail.com>
2026-04-18 23:53:47 +0200

    Init

---
+++ YOLOcide/YOLOcideApp.swift
@@ -0,0 +1,17 @@
+//
+//  YOLOcideApp.swift
+//  YOLOcide
+//
+//  Created by Antonio Javier Benítez Guijarro on 18/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct YOLOcideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YOLOcideTests/YOLOcideTests.swift
@@ -0,0 +1,16 @@
+//
+//  YOLOcideTests.swift
+//  YOLOcideTests
+//
+//  Created by Antonio Javier Benítez Guijarro on 18/04/2026.
+//
+
+import Testing
+
+struct YOLOcideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
